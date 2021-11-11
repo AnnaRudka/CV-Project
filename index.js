8dd418e336b5ab7970f9c2088dfccbee5e07a6df
AnnaRudka <anna.khomulyak@gmail.com>
2021-11-11 13:21:57 +0200

    1 version CV added

---
+++ index.js
@@ -0,0 +1,23 @@
+function move(elem) {
+  let i = 0;
+  let percent = parseInt(elem.dataset.percent, 10);
+  if (i == 0) {
+    i = 1;
+    let width = 10;
+    let id = setInterval(frame, 50);
+    function frame() {
+      if (width >= percent) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+
+move(document.getElementById("myBar"));
+move(document.getElementById("myBar2"));
+move(document.getElementById("myBar3"));
